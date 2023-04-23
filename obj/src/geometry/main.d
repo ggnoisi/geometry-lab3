50c19cbb6de6905ba036c94ed02256f4b6fddcc6
Mihail <ggnoese@mail.ru>
2023-04-23 19:43:43 +0700

    Restore geometry

--- obj/src/geometry/main.d
+++ obj/src/geometry/main.d
@@ -1,3 +1,3 @@
-obj/src/geometry/main.o: src/geometry/main.cpp src/libgeometry/lexer.cpp \
- src/libgeometry/lexer.h src/libgeometry/colors.h \
+obj/src/geometry/main.o: src/geometry/main.cpp src/libgeometry/colors.h \
+ src/libgeometry/lexer.cpp src/libgeometry/lexer.h \
  src/libgeometry/parser.cpp src/libgeometry/parser.h
